2b930792073570968ac49ab41f7e604552478f92
Tsatogua <timonhztv@gmail.com>
2021-02-22 17:37:20 +0300

    Added constructors to TileTest and _Move_tile function.

--- Scripts/TileTest.cs
+++ Scripts/TileTest.cs
@@ -4,11 +4,29 @@ using System;
 public class TileTest : MeshInstance
 {
     // Declare member variables here. Examples:
+    private void _Move_tile(int row, int column)
+    {
+        Vector3 new_coords;
+        if (Math.Abs(column) == 2)
+            new_coords = new Vector3((float)Math.Sqrt(3), 0, 0);
+        else
+            new_coords = new Vector3((float)Math.Sqrt(3)/2, 0, 1.5F);
+        this.Transform.Translated(new_coords);
+    }
     public int movement{get; private set;}
     // private unit unit_on_tile;
     public bool is_unit_on_tile{get; private set;}
-    private TileTest []touching_tiles = new TileTest[6];
+    private int []coordinats = new int[2];
+    TileTest(){
+        
+    }
 
+    TileTest(int row, int column)
+    {
+        coordinats[0] = row;
+        coordinats[1] = column;
+        _Move_tile(row, column);
+    }
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
