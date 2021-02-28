78386063dc73fe2f9670ce7a767f582919f980b3
Tsatogua <timonhztv@gmail.com>
2021-02-28 15:30:39 +0300

    Added tile_type property and method _Set_tiletype

--- Scripts/TileTest.cs
+++ Scripts/TileTest.cs
@@ -12,10 +12,40 @@ public class TileTest : MeshInstance
         new_coords = new Vector3(row*(float)Math.Sqrt(3)/2, 0, column*1.5F);
         this.Transform = Transform.Translated(new_coords);
     }
+    
+    public void _Set_tiletype(string type)
+    {
+        Mesh new_mesh;
+        switch (type)
+        {
+            case "Grass":
+                tile_type = type;
+                movement = 0;
+                new_mesh = GD.Load<Mesh>("res://Resources/Tiles/GrassTile.mesh");
+                break;
+            case "Mountain":
+                tile_type = type;
+                movement = -1488;
+                new_mesh = GD.Load<Mesh>("res://Resources/Tiles/MountainTile.mesh");
+                break;
+            case "Lake":
+                movement = -1;
+                new_mesh = GD.Load<Mesh>("res://Resources/Tiles/LakeTile.mesh");
+                break;
+            default:
+                tile_type = "Grass";
+                movement = 0;
+                new_mesh = GD.Load<Mesh>("res://Resources/Tiles/GrassTile.mesh");
+                break;
+        }
+        this.Mesh = new_mesh;
+    }
+    
     public int movement{get; private set;}
     // private unit unit_on_tile;
     public bool is_unit_on_tile{get; private set;}
     private int []coords = new int[2];
+    private string tile_type = "None";
     TileTest(){
         coords[0] = 0;
         coords[1] = 0;
