7890b0c3a9750acb6104fd11a197f24f495f7cff
Tsatogua <timonhztv@gmail.com>
2021-02-28 15:50:03 +0300

    Crated basic Map_Generation func.

--- Scripts/Map.cs
+++ Scripts/Map.cs
@@ -4,12 +4,13 @@ using System;
 public class Map : Spatial
 {
     // Declare member variables here. Examples:
-
+    private TileTest[,] map;
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    private void _Map_Generation()
     {
         var _tile_scene = GD.Load<PackedScene>("res://Scenes/TileTest.tscn");
         int amount_of_columns;
+        map = new TileTest[9, 9];
         for (int i = -4; i < 5; i ++)
         {
             if (i % 2 == 0) amount_of_columns = 9;
@@ -19,8 +20,13 @@ public class Map : Spatial
                 var _tile = _tile_scene.Instance() as TileTest;
                 AddChild(_tile);
                 _tile._Move_tile(i, j);
+                map[i + 4, j + amount_of_columns - 5] = _tile;
             }
         }
+    }
+    public override void _Ready()
+    {
+        _Map_Generation();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
