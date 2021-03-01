175c989a80a0e4dc95a4ec4c0677b5dca9aa6912
Tsatogua <timonhztv@gmail.com>
2021-03-01 18:01:38 +0300

    Enlargened the map and finished map generation

--- Scripts/Map.cs
+++ Scripts/Map.cs
@@ -10,26 +10,31 @@ public class Map : Spatial
     {
         var _tile_scene = GD.Load<PackedScene>("res://Scenes/TileTest.tscn");
         int amount_of_columns;
-        map = new TileTest[9, 9];
-        for (int i = -4; i < 5; i ++)
+        map = new TileTest[11, 11];
+        for (int i = -5; i < 6; i ++)
         {
-            if (i % 2 == 0) amount_of_columns = 9;
-            else amount_of_columns = 8;
-            for (int j = -(amount_of_columns - 5); j <= amount_of_columns - 5; j += 2)
+            if (i % 2 == 0) amount_of_columns = 11;
+            else amount_of_columns = 10;
+            for (int j = -(amount_of_columns - 6); j <= amount_of_columns - 6; j += 2)
             {
                 var _tile = _tile_scene.Instance() as TileTest;
                 AddChild(_tile);
                 _tile._Move_tile(i, j);
-                map[_tile.coord(0) + 4, _tile.coord(1) + 4] = _tile;
+                map[_tile.coord(0) + 5, _tile.coord(1) + 5] = _tile;
             }
         }
-        for (int i = 0; i < 9; i++)
-            for (int j = 0; j < 9; j ++)
+        for (int i = 0; i < 11; i++)
+            for (int j = 0; j < 11; j ++)
             {
             if (map[i, j] == null) continue;
-            if ((j <= 1) || (j >= 7)) map[i, j]._Set_Random_Tiletype(2);
+            if ((j <= 1) || (j >= 9) || (i <= 1) || (i >= 9)) map[i, j]._Set_Random_Tiletype(2);
             else map[i, j]._Set_Random_Tiletype(3);
             }
+    }
+
+    class Pathfinder
+    {
+
     }
     public override void _Ready()
     {
