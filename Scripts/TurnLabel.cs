21e8888be7658f4ce7967770a24633d7da16f544
Tsatogua <timonhztv@gmail.com>
2021-03-02 14:13:42 +0300

    Added turnchanging.

---
+++ Scripts/TurnLabel.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public class TurnLabel : Label
+{
+    // Declare member variables here. Examples:
+    int current_player = 2;
+    string current_controller = "AI";
+    string gamemode = "PvP";
+    public void _on_NextTurnButton_pressed()
+    {   
+        
+        switch (gamemode)
+        {
+            case "PvP":
+                if (current_player == 1) current_player++;
+                else current_player--;
+                this.Text = "Player" + " " + current_player.ToString() + "'s turn";
+                break;
+            case "PvE":
+                if (current_controller == "Player") current_controller = "AI";
+                else current_controller = "Player";
+                this.Text = current_controller + " " + "'s turn";
+                break;
+        }
+    }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        this.Text = "Press Next Turn Button to start";
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
