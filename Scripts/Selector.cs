23929541a9d16a6955dbb2b50d76bf6340568e88
Tsatogua <timonhztv@gmail.com>
2021-03-06 16:35:21 +0300

    Now unit moves to adjacent tile only

--- Scripts/Selector.cs
+++ Scripts/Selector.cs
@@ -6,6 +6,7 @@ public class Selector : Spatial
     // Declare member variables here. Examples:
     TileTest first;
     TileTest second;
+    int curr_player = 3;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -15,16 +16,30 @@ public class Selector : Spatial
     private void _on_Tile_selected(TileTest tile)
     {
         if (first == null && tile.unit_on_tile != null)
-        { 
-            first = tile;
-            GD.Print("first");
-        }
-        else if (first != null && tile.unit_on_tile == null)
         {
-            GD.Print("second");
-            first.unit_on_tile._Change_Tile(tile);
-            first = null;
+            if ((curr_player == 1 && tile.unit_on_tile.IsInGroup("Player1"))||(curr_player == 2 && tile.unit_on_tile.IsInGroup("Player2")))
+            { 
+                first = tile;
+                GD.Print("first " + tile.coord(0).ToString() + ';' + tile.coord(1).ToString());
+            }
+        }
+        else if (first != null && tile.unit_on_tile == null && (tile.movement != -1488))
+        {
+            int x1 = first.coord(0), x2 = tile.coord(0), z1 = first.coord(1), z2 = tile.coord(1);
+            if ((Math.Abs(x1 - x2) == 2 && z1 == z2) || (Math.Abs(x1 - x2) == 1 && Math.Abs(z1 - z2) == 1))
+            {
+                GD.Print("second "+ tile.coord(0).ToString() + ';' + tile.coord(1).ToString());
+                first.unit_on_tile._Change_Tile(tile);
+                first = null;
+            }
         }    
+    }
+
+    public void _Change_player()
+    {
+        if (curr_player == 1) curr_player ++;
+        else curr_player = 1;
+        GD.Print("Player"+ curr_player.ToString());
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
